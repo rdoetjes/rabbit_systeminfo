796b639b2318c8779ecdfd0be2932534c317234d
Raymond Doetjes <rdoetjes@phonax.com>
2023-04-15 06:10:10 +0200

    formatting

--- systeminfo/src/main.rs
+++ systeminfo/src/main.rs
@@ -1,11 +1,16 @@
-use std::{thread, time};
-use amqprs::{connection::{Connection, OpenConnectionArguments}, callbacks::{DefaultConnectionCallback, DefaultChannelCallback}, channel::{Channel, BasicPublishArguments}, BasicProperties};
+use ::sysinfo::{CpuExt, System, SystemExt};
+use amqprs::{
+    callbacks::{DefaultChannelCallback, DefaultConnectionCallback},
+    channel::{BasicPublishArguments, Channel},
+    connection::{Connection, OpenConnectionArguments},
+    BasicProperties,
+};
 use serde::Serialize;
-use ::sysinfo::{System, SystemExt, CpuExt};
 use serde_json;
+use std::{thread, time};
 
 #[derive(Default, Debug, Clone, Serialize)]
-struct SystemInfo{
+struct SystemInfo {
     tot_memory: u64,
     used_memory: u64,
     tot_swap: u64,
@@ -13,10 +18,10 @@ struct SystemInfo{
     cpu_util: Vec<f32>,
 }
 
-impl SystemInfo{
-    fn default() -> SystemInfo{
+impl SystemInfo {
+    fn default() -> SystemInfo {
         let mut sys = System::new_all();
-        // First we update all information of our `System` struct.   
+        // First we update all information of our `System` struct.
         sys.refresh_cpu();
         sys.refresh_memory();
 
@@ -29,27 +34,50 @@ impl SystemInfo{
 
 async fn connect_rabbitmq(connection_details: &RabbitConnect) -> Connection {
     //this is for demo and teaching purposes, you would fetch this information from a config of course
-    let mut res = Connection::open(&OpenConnectionArguments::new(&connection_details.host, connection_details.port, &connection_details.username, &connection_details.password)).await;
+    let mut res = Connection::open(&OpenConnectionArguments::new(
+        &connection_details.host,
+        connection_details.port,
+        &connection_details.username,
+        &connection_details.password,
+    ))
+    .await;
 
-    while res.is_err(){
+    while res.is_err() {
         println!("trying to connect after error");
         std::thread::sleep(time::Duration::from_millis(2000));
-        res =  Connection::open(&OpenConnectionArguments::new(&connection_details.host, connection_details.port, &connection_details.username, &connection_details.password)).await;
+        res = Connection::open(&OpenConnectionArguments::new(
+            &connection_details.host,
+            connection_details.port,
+            &connection_details.username,
+            &connection_details.password,
+        ))
+        .await;
     }
 
     let connection = res.unwrap();
-    connection.register_callback(DefaultConnectionCallback).await.unwrap();
+    connection
+        .register_callback(DefaultConnectionCallback)
+        .await
+        .unwrap();
     connection
 }
 
-async fn channel_rabbitmq(connection: &amqprs::connection::Connection)-> Channel{
+async fn channel_rabbitmq(connection: &amqprs::connection::Connection) -> Channel {
     let channel = connection.open_channel(None).await.unwrap();
-    channel.register_callback(DefaultChannelCallback).await.unwrap();
-    return  channel;
+    channel
+        .register_callback(DefaultChannelCallback)
+        .await
+        .unwrap();
+    return channel;
 }
 
-async fn send(connection: &mut amqprs::connection::Connection, channel: &mut Channel,connection_details: &RabbitConnect, result: &str){
-    if !connection.is_open(){
+async fn send(
+    connection: &mut amqprs::connection::Connection,
+    channel: &mut Channel,
+    connection_details: &RabbitConnect,
+    result: &str,
+) {
+    if !connection.is_open() {
         println!("Connection not open");
         *connection = connect_rabbitmq(connection_details).await;
         *channel = channel_rabbitmq(&connection).await;
@@ -59,34 +87,42 @@ async fn send(connection: &mut amqprs::connection::Connection, channel: &mut Cha
     if !channel.is_open() {
         println!("channel is not open, does exchange systemmonitor exist on rabbitMQ?");
         *channel = channel_rabbitmq(&connection).await;
-    }
-    else {
+    } else {
         let args = BasicPublishArguments::new("systemmonitor", "");
-        channel.basic_publish(BasicProperties::default(), result.into(), args).await.unwrap();
+        channel
+            .basic_publish(BasicProperties::default(), result.into(), args)
+            .await
+            .unwrap();
     }
 }
 
-async fn get_sys_info(connection: &mut amqprs::connection::Connection, channel: &mut Channel, connection_details: &RabbitConnect, sys: &mut System, details: &mut SystemInfo){
+async fn get_sys_info(
+    connection: &mut amqprs::connection::Connection,
+    channel: &mut Channel,
+    connection_details: &RabbitConnect,
+    sys: &mut System,
+    details: &mut SystemInfo,
+) {
+    sys.refresh_cpu();
+    sys.refresh_memory();
 
-        sys.refresh_cpu();
-        sys.refresh_memory();
-        
-        details.tot_memory = sys.total_memory();
-        details.used_memory = sys.used_memory();
-        details.tot_swap = sys.total_swap();
-        details.used_swap = sys.used_swap();
+    details.tot_memory = sys.total_memory();
+    details.used_memory = sys.used_memory();
+    details.tot_swap = sys.total_swap();
+    details.used_swap = sys.used_swap();
 
-        details.cpu_util.clear();
-        for cpu in sys.cpus() {
-            details.cpu_util.push(cpu.cpu_usage()); 
-        }
+    details.cpu_util.clear();
+    for cpu in sys.cpus() {
+        details.cpu_util.push(cpu.cpu_usage());
+    }
 
-        let result = serde_json::to_string(&details.to_owned()).expect("{}").to_string();
-        send(connection, channel, connection_details, &result).await;
-
+    let result = serde_json::to_string(&details.to_owned())
+        .expect("{}")
+        .to_string();
+    send(connection, channel, connection_details, &result).await;
 }
 
-struct RabbitConnect{
+struct RabbitConnect {
     host: String,
     port: u16,
     username: String,
@@ -95,16 +131,27 @@ struct RabbitConnect{
 
 #[tokio::main]
 async fn main() {
-    let connection_details = RabbitConnect{host: "localhost".to_string(), port: 5672, username: "consumer".to_string(), password: "crabs".to_string(),};
+    let connection_details = RabbitConnect {
+        host: "localhost".to_string(),
+        port: 5672,
+        username: "consumer".to_string(),
+        password: "crabs".to_string(),
+    };
     let mut sys = System::new_all();
     let mut details = SystemInfo::default();
-   
+
     let mut connection = connect_rabbitmq(&connection_details).await;
     let mut channel = channel_rabbitmq(&connection).await;
 
-    loop{
-        get_sys_info(&mut connection, &mut channel, &connection_details, &mut sys, &mut details).await;
-        thread::sleep(time::Duration::from_millis(1000));     
+    loop {
+        get_sys_info(
+            &mut connection,
+            &mut channel,
+            &connection_details,
+            &mut sys,
+            &mut details,
+        )
+        .await;
+        thread::sleep(time::Duration::from_millis(1000));
     }
-    
 }
--- systeminfo_client/src/main.rs
+++ systeminfo_client/src/main.rs
@@ -1,65 +1,86 @@
-use std::{time};
-use amqprs::{connection::{Connection, OpenConnectionArguments}, callbacks::{DefaultConnectionCallback, DefaultChannelCallback}, channel::{Channel, QueueBindArguments, BasicConsumeArguments, BasicCancelArguments, QueueDeclareArguments, BasicAckArguments}};
+use amqprs::{
+    callbacks::{DefaultChannelCallback, DefaultConnectionCallback},
+    channel::{
+        BasicAckArguments, BasicCancelArguments, BasicConsumeArguments, Channel,
+        QueueBindArguments, QueueDeclareArguments,
+    },
+    connection::{Connection, OpenConnectionArguments},
+};
+use std::time;
 use uuid::Uuid;
 
 async fn connect_rabbitmq(connection_details: &RabbitConnect) -> Connection {
     //this is for demo and teaching purposes, you would fetch this information from a config of course
-    let mut res = Connection::open(&OpenConnectionArguments::new(&connection_details.host, connection_details.port, &connection_details.username, &connection_details.password)).await;
+    let mut res = Connection::open(&OpenConnectionArguments::new(
+        &connection_details.host,
+        connection_details.port,
+        &connection_details.username,
+        &connection_details.password,
+    ))
+    .await;
 
-    while res.is_err(){
+    while res.is_err() {
         println!("trying to connect after error");
         std::thread::sleep(time::Duration::from_millis(2000));
-        res =  Connection::open(&OpenConnectionArguments::new(&connection_details.host, connection_details.port, &connection_details.username, &connection_details.password)).await;
+        res = Connection::open(&OpenConnectionArguments::new(
+            &connection_details.host,
+            connection_details.port,
+            &connection_details.username,
+            &connection_details.password,
+        ))
+        .await;
     }
 
     let connection = res.unwrap();
-    connection.register_callback(DefaultConnectionCallback).await.unwrap();
+    connection
+        .register_callback(DefaultConnectionCallback)
+        .await
+        .unwrap();
     connection
 }
 
-async fn channel_rabbitmq(connection: &amqprs::connection::Connection)-> Channel{
+async fn channel_rabbitmq(connection: &amqprs::connection::Connection) -> Channel {
     let channel = connection.open_channel(None).await.unwrap();
-    channel.register_callback(DefaultChannelCallback).await.unwrap();
-    return  channel;
+    channel
+        .register_callback(DefaultChannelCallback)
+        .await
+        .unwrap();
+    return channel;
 }
 
-async fn bind_queue_to_exchange(connection: &mut amqprs::connection::Connection, channel: &mut Channel,connection_details: &RabbitConnect, queue: &str){
-    if !connection.is_open(){
+async fn bind_queue_to_exchange(
+    connection: &mut amqprs::connection::Connection,
+    channel: &mut Channel,
+    connection_details: &RabbitConnect,
+    queue: &str,
+) {
+    if !connection.is_open() {
         println!("Connection not open");
         *connection = connect_rabbitmq(connection_details).await;
         *channel = channel_rabbitmq(&connection).await;
         println!("{}", connection);
     }
-  
+
     let qparams = QueueDeclareArguments::default()
         .queue(queue.to_owned())
         .auto_delete(true)
         .durable(false)
         .finish();
 
-    let (queue, _, _) = channel
-        .queue_declare(qparams)
-        .await
-        .unwrap()
-        .unwrap();
+    let (queue, _, _) = channel.queue_declare(qparams).await.unwrap().unwrap();
 
     if !channel.is_open() {
         println!("channel is not open, does exchange systemmonitor exist on rabbitMQ?");
         *channel = channel_rabbitmq(&connection).await;
     }
-   
+
     channel
-        .queue_bind(QueueBindArguments::new(
-            &queue,
-            "systemmonitor",
-            "",
-        ))
+        .queue_bind(QueueBindArguments::new(&queue, "systemmonitor", ""))
         .await
         .unwrap();
-
 }
 
-struct RabbitConnect{
+struct RabbitConnect {
     host: String,
     port: u16,
     username: String,
@@ -68,22 +89,24 @@ struct RabbitConnect{
 
 #[tokio::main]
 async fn main() {
-    let connection_details = RabbitConnect{host: "localhost".to_string(), port: 5672, username: "consumer".to_string(), password: "crabs".to_string(),};   
-  
+    let connection_details = RabbitConnect {
+        host: "localhost".to_string(),
+        port: 5672,
+        username: "consumer".to_string(),
+        password: "crabs".to_string(),
+    };
+
     // create a unique queue and bind it to the exchange systemmonitor
     let uuid = Uuid::new_v4();
     let queue = format!("c_systemmonitor_{}", uuid.as_hyphenated().to_string());
-    let args = BasicConsumeArguments::new(
-        &queue,
-        format!("{} sub_monitor", queue).as_str()
-    );
-    
+    let args = BasicConsumeArguments::new(&queue, format!("{} sub_monitor", queue).as_str());
+
     //this loop makes sure that on error we do a whole new reconnect and setup of the new queue and consumer/error structs
     //it will try to reconnect every two seconds in the connect_rabbitmq
     //when a connection is made it will create a new channel on that connection and bind the queue to the exchange and spawn an new worker
     //you can also put all connect logic in its own function and call it during setup and when the tokio join returns a Err option this would be a cleane approach
     //but for the sake of teaching the concept a bit convoluted, hence the loop without the actual Err action
-    loop{
+    loop {
         let mut connection = connect_rabbitmq(&connection_details).await;
         let mut channel = channel_rabbitmq(&connection).await;
 
@@ -92,15 +115,15 @@ async fn main() {
         let (ctag, mut messages_rx) = channel.basic_consume_rx(args.clone()).await.unwrap();
 
         //this is the actual worker logic, spawed with ansync tokio process
-        let join = tokio::spawn( async move {
+        let join = tokio::spawn(async move {
             while let Some(msg) = messages_rx.recv().await {
                 let a = msg.content.unwrap();
                 let s = String::from_utf8_lossy(&a);
-                
+
                 //call your own function and do something usefull and return Ok or Err and on Ok ack the message, this way you don't loose messages
                 //this is assuming there are no symatic errors in the message in that case when the message needs to be discarded also call ack.
                 //but that is up to your functional error handling
-                println!("{}", s); 
+                println!("{}", s);
 
                 let args = BasicAckArguments::new(msg.deliver.unwrap().delivery_tag(), false);
                 let _ = channel.basic_ack(args).await;
